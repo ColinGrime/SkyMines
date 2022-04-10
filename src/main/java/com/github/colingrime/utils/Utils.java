4e64f627fe1b65c5739037d0c37d942beebb496b
Scill <realscill@gmail.com>
2022-04-10 19:56:03 -0400

    Add location parser

--- src/main/java/com/github/colingrime/utils/Utils.java
+++ src/main/java/com/github/colingrime/utils/Utils.java
@@ -3,8 +3,7 @@ package com.github.colingrime.utils;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.apache.commons.lang.WordUtils;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -85,5 +84,67 @@ public final class Utils {
 		} else {
 			player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
 		}
+	}
+
+	/**
+	 * @param location any location
+	 * @return string form of location
+	 */
+	public static String parseLocation(Location location) {
+		String world = location.getWorld().getName();
+		String x = String.valueOf(location.getX());
+		String y = String.valueOf(location.getY());
+		String z = String.valueOf(location.getZ());
+		String yaw = String.valueOf(location.getYaw());
+		String pitch = String.valueOf(location.getPitch());
+		return putBreakers(world, x, y, z, yaw, pitch);
+	}
+
+	/**
+	 * @param text any text
+	 * @return location form of string
+	 */
+	public static Location parseLocation(String text) {
+		String[] args = text.split(":");
+		if (args.length < 4) {
+			return null;
+		}
+
+		World world = Bukkit.getWorld(args[0]);
+		double x, y, z;
+		float yaw = 0, pitch = 0;
+
+		try {
+			x = Double.parseDouble(args[1]);
+			y = Double.parseDouble(args[2]);
+			z = Double.parseDouble(args[3]);
+
+			if (args.length == 6) {
+				yaw = Float.parseFloat(args[4]);
+				pitch = Float.parseFloat(args[5]);
+			}
+		} catch (NumberFormatException ex) {
+			return null;
+		}
+
+		return new Location(world, x, y, z, yaw, pitch);
+	}
+
+	/**
+	 * @param text any amount of text
+	 * @return the text wrapped in a single string with colons between them
+	 */
+	private static String putBreakers(String...text) {
+		StringBuilder builder = new StringBuilder();
+		for (int i=0; i<text.length; i++) {
+			builder.append(text[i]);
+
+			// if it's not the last string
+			if (i != text.length - 1) {
+				builder.append(":");
+			}
+		}
+
+		return builder.toString();
 	}
 }
