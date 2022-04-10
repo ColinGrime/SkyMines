b16559be0ff7dc9b2aede8ce4d5741019900d6d3
Scill <realscill@gmail.com>
2022-04-10 19:56:55 -0400

    Add storage type info + parse method

--- src/main/java/com/github/colingrime/storage/StorageType.java
+++ src/main/java/com/github/colingrime/storage/StorageType.java
@@ -2,7 +2,28 @@ package com.github.colingrime.storage;
 
 public enum StorageType {
 
-	FlatFile,
-	MySql,
-	None
+	YAML("YAML"),
+	MYSQL("MySQL");
+	// TODO add more storage types
+
+	private final String name;
+
+	StorageType(String name) {
+		this.name = name;
+	}
+
+	public static StorageType parse(String name) {
+		for (StorageType type : StorageType.values()) {
+			if (type.getName().equalsIgnoreCase(name)) {
+				return type;
+			}
+		}
+
+		// default storage type
+		return StorageType.YAML;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
