5c4e3c9733a6d18ff6bdb6f411f219927a27682a
Scill <realscill@gmail.com>
2022-04-10 20:03:44 -0400

    Add connection providers

---
+++ src/main/java/com/github/colingrime/storage/sql/connection/hikari/MySqlConnectionProvider.java
@@ -0,0 +1,49 @@
+package com.github.colingrime.storage.sql.connection.hikari;
+
+import com.github.colingrime.storage.StorageCredentials;
+import com.zaxxer.hikari.HikariConfig;
+
+import java.util.Map;
+import java.util.function.Function;
+
+public class MySqlConnectionProvider extends HikariConnectionProvider {
+
+	public MySqlConnectionProvider(StorageCredentials credentials) {
+		super(credentials);
+	}
+
+	@Override
+	public String getName() {
+		return "MySQL";
+	}
+
+	@Override
+	public Function<String, String> getStatementProcessor() {
+		return s -> s.replace('\'', '`');
+	}
+
+	@Override
+	protected void configureDatabase(HikariConfig config, StorageCredentials credentials) {
+		config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		config.setJdbcUrl("jdbc:mysql://" + credentials.getHost() + ":" + credentials.getPort() + "/" + credentials.getDatabase());
+		config.setUsername(credentials.getUsername());
+		config.setPassword(credentials.getPassword());
+	}
+
+	@Override
+	protected void overrideProperties(Map<String, String> properties) {
+		// https://github.com/brettwooldridge/HikariCP/wiki/MySQL-Configuration
+		properties.putIfAbsent("cachePrepStmts", "true");
+		properties.putIfAbsent("prepStmtCacheSize", "250");
+		properties.putIfAbsent("prepStmtCacheSqlLimit", "2048");
+		properties.putIfAbsent("useServerPrepStmts", "true");
+		properties.putIfAbsent("useLocalSessionState", "true");
+		properties.putIfAbsent("rewriteBatchedStatements", "true");
+		properties.putIfAbsent("cacheResultSetMetadata", "true");
+		properties.putIfAbsent("cacheServerConfiguration", "true");
+		properties.putIfAbsent("elideSetAutoCommits", "true");
+		properties.putIfAbsent("maintainTimeStats", "false");
+
+		super.overrideProperties(properties);
+	}
+}
