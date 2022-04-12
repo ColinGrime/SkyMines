7b8aa9a6e0681dba0c46d017552167f4fafca4a2
Scill <realscill@gmail.com>
2022-04-12 17:44:17 -0400

    Add command option

--- src/main/java/com/github/colingrime/panel/setup/slot/meta/PanelSlotMeta.java
+++ src/main/java/com/github/colingrime/panel/setup/slot/meta/PanelSlotMeta.java
@@ -10,15 +10,21 @@ public class PanelSlotMeta {
 	private final Material type;
 	private final String name;
 	private final List<String> lore;
+	private final String command;
 
 	public PanelSlotMeta(Material type, String name) {
-		this(type, name, new ArrayList<>());
+		this(type, name, new ArrayList<>(), "");
 	}
 
 	public PanelSlotMeta(Material type, String name, List<String> lore) {
+		this(type, name, lore, "");
+	}
+
+	public PanelSlotMeta(Material type, String name, List<String> lore, String command) {
 		this.type = type;
 		this.name = name;
 		this.lore = lore;
+		this.command = command;
 	}
 
 	public Material getType() {
@@ -31,5 +37,9 @@ public class PanelSlotMeta {
 
 	public List<String> getLore() {
 		return lore;
+	}
+
+	public String getCommand() {
+		return command;
 	}
 }
