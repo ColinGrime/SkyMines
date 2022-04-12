048e681996b0c84196f7c375a9799c2511491122
Scill <realscill@gmail.com>
2022-04-12 17:48:47 -0400

    Add max skymine limit and skymine setup on join

--- src/main/java/com/github/colingrime/listeners/PlayerListeners.java
+++ src/main/java/com/github/colingrime/listeners/PlayerListeners.java
@@ -2,17 +2,20 @@ package com.github.colingrime.listeners;
 
 import com.github.colingrime.SkyMines;
 import com.github.colingrime.locale.Messages;
-import com.github.colingrime.panel.UpgradePanel;
+import com.github.colingrime.panel.MainPanel;
 import com.github.colingrime.skymines.SkyMine;
+import com.github.colingrime.skymines.manager.SkyMineManager;
 import com.github.colingrime.skymines.structure.MineSize;
 import com.github.colingrime.skymines.token.SkyMineToken;
 import com.github.colingrime.skymines.upgrades.SkyMineUpgrades;
 import com.github.colingrime.utils.Utils;
+import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
@@ -31,31 +34,45 @@ public class PlayerListeners implements Listener {
 			return;
 		}
 
+		SkyMineManager manager = plugin.getSkyMineManager();
 		Player player = event.getPlayer();
-		ItemStack item = player.getInventory().getItemInMainHand();
-		SkyMineToken token = plugin.getSkyMineManager().getToken();
-
-		if (token.isToken(item)) {
-			MineSize size = token.getMineSize(item).orElseGet(() -> new MineSize(10, 10, 10));
-			SkyMineUpgrades upgrades = token.getUpgrades(item).orElseGet(() -> new SkyMineUpgrades(plugin));
-
-			if (plugin.getSkyMineManager().createSkyMine(player, player.getLocation().subtract(0, 1, 0), size, upgrades)) {
-				Utils.removeOneItemFromHand(player);
-			} else {
-				Messages.FAILURE_NO_SPACE.sendTo(player);
-			}
-
-			return;
-		}
-
 		Block block = event.getClickedBlock();
+
 		if (block != null) {
-			for (SkyMine skyMine : plugin.getSkyMineManager().getSkyMines(player)) {
+			for (SkyMine skyMine : manager.getSkyMines(player)) {
 				if (skyMine.getStructure().getParameter().contains(block.getLocation().toVector())) {
-					new UpgradePanel(plugin, player, skyMine).openInventory(player);
+					new MainPanel(plugin, player, skyMine).openInventory(player);
 					return;
 				}
 			}
 		}
+
+		ItemStack item = player.getInventory().getItemInMainHand();
+		SkyMineToken token = manager.getToken();
+
+		if (token.isToken(item)) {
+			if (manager.getSkyMines(player).size() > plugin.getSettings().getMaxPerPlayer()) {
+				Messages.FAILURE_MAX_AMOUNT.sendTo(player);
+				return;
+			}
+
+			MineSize size = token.getMineSize(item).orElseGet(() -> new MineSize(10, 10, 10));
+			SkyMineUpgrades upgrades = token.getUpgrades(item).orElseGet(() -> new SkyMineUpgrades(plugin));
+
+			if (manager.createSkyMine(player, player.getLocation().subtract(0, 1, 0), size, upgrades)) {
+				Utils.removeOneItemFromHand(player);
+			} else {
+				Messages.FAILURE_NO_SPACE.sendTo(player);
+			}
+		}
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+			for (SkyMine skyMine : plugin.getSkyMineManager().getSkyMines(event.getPlayer())) {
+				skyMine.getStructure().setup();
+			}
+		});
 	}
 }
