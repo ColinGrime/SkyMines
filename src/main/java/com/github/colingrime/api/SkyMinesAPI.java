b2e9fe85f4874d88063151834d8a19d4e71bf6d8
Scill <realscill@gmail.com>
2022-04-13 09:18:33 -0400

    Add manager to API

--- src/main/java/com/github/colingrime/api/SkyMinesAPI.java
+++ src/main/java/com/github/colingrime/api/SkyMinesAPI.java
@@ -1,23 +1,26 @@
 package com.github.colingrime.api;
 
 import com.github.colingrime.SkyMines;
+import com.github.colingrime.skymines.manager.SkyMineManager;
 
-public class SkyMinesAPI {
+public final class SkyMinesAPI {
 
 	private static final SkyMinesAPI instance;
-	private SkyMines plugin;
+	private final SkyMines plugin;
 
 	static {
 		instance = new SkyMinesAPI();
 	}
 
-	private SkyMinesAPI() {}
+	private SkyMinesAPI() {
+		this.plugin = SkyMines.getInstance();
+	}
 
 	public static SkyMinesAPI getInstance() {
 		return instance;
 	}
 
-	public void setPlugin(SkyMines plugin) {
-		this.plugin = plugin;
+	public SkyMineManager getManager() {
+		return plugin.getSkyMineManager();
 	}
 }
