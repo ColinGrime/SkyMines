11c492313bca93f4369bc94e1ee6fb50b8da6760
Scill <realscill@gmail.com>
2022-04-13 09:17:24 -0400

    Check distance on sethome to ensure player is close to mine

--- src/main/java/com/github/colingrime/commands/skymines/subcommands/SkyMinesSetHomeSubCommand.java
+++ src/main/java/com/github/colingrime/commands/skymines/subcommands/SkyMinesSetHomeSubCommand.java
@@ -4,19 +4,37 @@ import com.github.colingrime.SkyMines;
 import com.github.colingrime.commands.skymines.SkyMinesSubCommand;
 import com.github.colingrime.locale.Messages;
 import com.github.colingrime.skymines.SkyMine;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 public class SkyMinesSetHomeSubCommand extends SkyMinesSubCommand {
 
+	private final SkyMines plugin;
+
 	public SkyMinesSetHomeSubCommand(SkyMines plugin) {
 		super(plugin);
+		this.plugin = plugin;
 	}
 
 	@Override
 	public void onCommand(CommandSender sender, String[] args, SkyMine skyMine) {
-		skyMine.setHome(((Player) sender).getLocation());
-		Messages.SUCCESS_SETHOME.sendTo(sender);
+		Location location = ((Player) sender).getLocation();
+		Vector playerVector = location.toVector();
+
+		Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+			for (Vector vector : skyMine.getStructure().getParameter()) {
+				if (vector.distance(playerVector) <= skyMine.getStructure().getSize().getMaxSide() / 2.0) {
+					skyMine.setHome(location);
+					Messages.SUCCESS_SETHOME.sendTo(sender);
+					return;
+				}
+			}
+
+			Messages.FAILURE_TOO_FAR_AWAY.sendTo(sender);
+		});
 	}
 
 	@Override
@@ -31,7 +49,7 @@ public class SkyMinesSetHomeSubCommand extends SkyMinesSubCommand {
 
 	@Override
 	public Messages getUsage() {
-		return Messages.USAGE_SKYMINES_HOME;
+		return Messages.USAGE_SKYMINES_SETHOME;
 	}
 
 	@Override
