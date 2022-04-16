3d4eca69fd25b24308f89409824a93d1e912c08c
Scill <realscill@gmail.com>
2022-04-15 23:47:36 -0400

    Add update checker

---
+++ src/main/java/com/github/colingrime/updater/SpigotUpdater.java
@@ -0,0 +1,33 @@
+package com.github.colingrime.updater;
+
+import com.github.colingrime.SkyMines;
+import com.github.colingrime.utils.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class SpigotUpdater {
+
+	private static final String UPDATE_URL = "https://api.spigotmc.org/legacy/update.php?resource=101373";
+	private static final String RESOURCE_URL = "https://www.spigotmc.org/resources/skymines.101373/";
+
+	private final SkyMines plugin;
+
+	public SpigotUpdater(SkyMines plugin) {
+		this.plugin = plugin;
+	}
+
+	public void checkForUpdate() {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(UPDATE_URL).openStream()))) {
+			String latestVersion = reader.readLine();
+			if (!plugin.getDescription().getVersion().equals(latestVersion)) {
+				Logger.warn("A new update has been found (v" + latestVersion + ")");
+				Logger.warn("Download it here: " + RESOURCE_URL);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
