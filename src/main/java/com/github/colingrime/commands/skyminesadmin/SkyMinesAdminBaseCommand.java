dd44d14b76d51769ff8e07017d9d658164524cde
Scill <realscill@gmail.com>
2022-04-17 02:51:35 -0400

    Add pickup (admin) subcommand

--- src/main/java/com/github/colingrime/commands/skyminesadmin/SkyMinesAdminBaseCommand.java
+++ src/main/java/com/github/colingrime/commands/skyminesadmin/SkyMinesAdminBaseCommand.java
@@ -3,10 +3,7 @@ package com.github.colingrime.commands.skyminesadmin;
 import com.github.colingrime.SkyMines;
 import com.github.colingrime.commands.BaseCommand;
 import com.github.colingrime.commands.SubCommand;
-import com.github.colingrime.commands.skyminesadmin.subcommands.SkyMinesGiveSubCommand;
-import com.github.colingrime.commands.skyminesadmin.subcommands.SkyMinesLookupSubCommand;
-import com.github.colingrime.commands.skyminesadmin.subcommands.SkyMinesReloadSubCommand;
-import com.github.colingrime.commands.skyminesadmin.subcommands.SkyMinesRemoveSubCommand;
+import com.github.colingrime.commands.skyminesadmin.subcommands.*;
 import com.github.colingrime.locale.Messages;
 
 import java.util.List;
@@ -24,9 +21,10 @@ public class SkyMinesAdminBaseCommand extends BaseCommand {
 
 	@Override
 	public void registerSubCommands(List<SubCommand> subCommands, SkyMines plugin) {
-		subCommands.add(new SkyMinesGiveSubCommand(plugin));
-		subCommands.add(new SkyMinesLookupSubCommand(plugin));
-		subCommands.add(new SkyMinesRemoveSubCommand(plugin));
-		subCommands.add(new SkyMinesReloadSubCommand(plugin));
+		subCommands.add(new GiveSubCommand(plugin));
+		subCommands.add(new LookupSubCommand(plugin));
+		subCommands.add(new PickupSubCommand(plugin));
+		subCommands.add(new RemoveSubCommand(plugin));
+		subCommands.add(new ReloadSubCommand(plugin));
 	}
 }
