5a5cf5bacea89bc27921d8f0c8d0fd2a8ae1a144
Scill <realscill@gmail.com>
2022-04-18 16:11:06 -0400

    Remove unused Listener

--- src/main/java/com/github/colingrime/panel/Panel.java
+++ src/main/java/com/github/colingrime/panel/Panel.java
@@ -14,7 +14,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class Panel implements Listener, InventoryHolder {
+public abstract class Panel implements InventoryHolder {
 
 	private final static Map<Player, Panel> panelViewers = new HashMap<>();
 
