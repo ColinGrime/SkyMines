41acff7c18b26af9443c03c87c16609c9479f3b8
Scill <realscill@gmail.com>
2022-06-04 11:01:45 -0400

    Add WorldGuard check

--- src/main/java/com/github/colingrime/skymines/structure/MineStructure.java
+++ src/main/java/com/github/colingrime/skymines/structure/MineStructure.java
@@ -1,13 +1,20 @@
 package com.github.colingrime.skymines.structure;
 
 import com.github.colingrime.SkyMines;
+import com.github.colingrime.skymines.structure.behavior.BuildBehavior;
 import com.github.colingrime.skymines.structure.material.MaterialSingle;
 import com.github.colingrime.skymines.structure.material.MaterialType;
-import com.github.colingrime.skymines.structure.material.MaterialVariety;
-import com.github.colingrime.skymines.structure.behavior.BuildBehavior;
-import com.github.colingrime.skymines.structure.region.CuboidRegion;
-import com.github.colingrime.skymines.structure.region.ParameterRegion;
-import com.github.colingrime.utils.Utils;
+import com.github.colingrime.skymines.structure.region.implementation.CuboidRegion;
+import com.github.colingrime.skymines.structure.region.implementation.ParameterRegion;
+import com.github.colingrime.utils.LocationUtils;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldguard.protection.flags.Flags;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -21,6 +28,7 @@ import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class MineStructure {
 
@@ -33,13 +41,16 @@ public class MineStructure {
 	private final ParameterRegion parameter;
 	private final CuboidRegion inside;
 
+	// if they have WG regions, they can hook into it
+	private ProtectedRegion worldGuardRegion;
+
 	public MineStructure(Location startCorner, Location endCorner, MineSize size, Material borderType) {
 		this.world = startCorner.getWorld();
 		this.startCorner = startCorner;
 		this.endCorner = endCorner;
 		this.size = size;
 		this.borderType = borderType;
-		this.parameter = new ParameterRegion(startCorner.toVector(), endCorner.toVector());
+		this.parameter = ParameterRegion.of(startCorner.toVector(), endCorner.toVector());
 		this.inside = _getInside();
 	}
 
@@ -61,10 +72,45 @@ public class MineStructure {
 		int maxY = minY == y1 ? y2 : y1;
 		int maxZ = minZ == z1 ? z2 : z1;
 
-		CuboidRegion inside = new CuboidRegion();
-		inside.setMin(new Vector(minX + 1, minY + 1, minZ + 1));
-		inside.setMax(new Vector(maxX - 1, maxY, maxZ - 1));
-		return inside;
+		Vector pt1 = new Vector(minX + 1, minY + 1, minZ + 1);
+		Vector pt2 = new Vector(maxX - 1, maxY, maxZ - 1);
+
+		return CuboidRegion.of(pt1, pt2);
+	}
+
+	/**
+	 * Checks for the WorldGuard hook.
+	 *
+	 * If it exists, a WorldGuard region
+	 * gets created inside the mine.
+	 *
+	 * This allows us other plugins to interact
+	 * with the inner mine (e.g. TokenEnchants).
+	 *
+	 * @param mineUUID UUID of the mine
+	 */
+	public void checkForWorldGuardHook(String mineUUID) {
+		RegionContainer container = SkyMines.getInstance().getDependencyManager().getRegionContainer();
+		if (container == null) {
+			return;
+		}
+
+		com.sk89q.worldedit.world.World world = BukkitAdapter.adapt(this.world);
+		RegionManager manager = container.get(world);
+		if (manager == null) {
+			return;
+		}
+
+		worldGuardRegion = manager.getRegion(mineUUID);
+		if (worldGuardRegion != null) {
+			return;
+		}
+
+		BlockVector3 vector1 = BlockVector3.at(startCorner.getX(), startCorner.getY(), startCorner.getZ());
+		BlockVector3 vector2 = BlockVector3.at(endCorner.getX(), endCorner.getY(), endCorner.getZ());
+		worldGuardRegion = new ProtectedCuboidRegion(mineUUID, vector1, vector2);
+		worldGuardRegion.setFlag(Flags.BLOCK_BREAK, StateFlag.State.ALLOW);
+		manager.addRegion(worldGuardRegion);
 	}
 
 	/**
@@ -115,14 +161,25 @@ public class MineStructure {
 		getBehavior().build(world, parameter, new MaterialSingle(borderType));
 	}
 
-	public void buildInside(MaterialVariety blockVariety) {
-		getBehavior().build(world, inside, blockVariety, SkyMines.getInstance().getSettings().shouldReplaceBlocks());
+	public void buildInside(MaterialType materialType) {
+		getBehavior().build(world, inside, materialType, SkyMines.getInstance().getSettings().shouldReplaceBlocks());
 	}
 
-	public void destroy() {
+	public void destroy(String mineUUID) {
 		MaterialType air = new MaterialSingle(Material.AIR);
 		getBehavior().build(world, parameter, air);
 		getBehavior().build(world, inside, air);
+
+		// destroy WG region if it exists
+		if (worldGuardRegion != null) {
+			RegionContainer container = SkyMines.getInstance().getDependencyManager().getRegionContainer();
+			com.sk89q.worldedit.world.World world = BukkitAdapter.adapt(this.world);
+			RegionManager manager = container.get(world);
+
+			if (manager != null) {
+				manager.removeRegion(mineUUID);
+			}
+		}
 	}
 
 	private BuildBehavior getBehavior() {
@@ -146,9 +203,9 @@ public class MineStructure {
 	}
 
 	public static String serialize(MineStructure structure) {
-		String startCorner = Utils.parseLocation(structure.startCorner);
-		String endCorner = Utils.parseLocation(structure.endCorner);
-		String size = MineSize.parse(structure.size);
+		String startCorner = LocationUtils.serializeLocation(structure.startCorner);
+		String endCorner = LocationUtils.serializeLocation(structure.endCorner);
+		String size = MineSize.serialize(structure.size);
 		String borderType = structure.borderType.name();
 		return startCorner + '\n' + endCorner + '\n' + size + '\n' + borderType;
 	}
@@ -159,15 +216,15 @@ public class MineStructure {
 			return null;
 		}
 
-		Location startCorner = Utils.parseLocation(texts[0]);
-		Location endCorner = Utils.parseLocation(texts[1]);
-		MineSize size = MineSize.parse(texts[2]);
+		Optional<Location> startCorner = LocationUtils.deserializeLocation(texts[0]);
+		Optional<Location> endCorner = LocationUtils.deserializeLocation(texts[1]);
+		MineSize size = MineSize.deserialize(texts[2]);
 		Material borderType = texts.length == 4 ? Material.getMaterial(texts[3]) : Material.BEDROCK;
 
-		if (startCorner == null || endCorner == null || size == null) {
+		if (startCorner.isEmpty() || endCorner.isEmpty() || size == null) {
 			return null;
 		}
 
-		return new MineStructure(startCorner, endCorner, size, borderType);
+		return new MineStructure(startCorner.get(), endCorner.get(), size, borderType);
 	}
 }
