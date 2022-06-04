89577fb344708eee7df8b4f22f18505e7997ba9b
Scill <realscill@gmail.com>
2022-06-04 10:46:45 -0400

    Require non-null log messages

--- src/main/java/com/github/colingrime/utils/Logger.java
+++ src/main/java/com/github/colingrime/utils/Logger.java
@@ -2,21 +2,28 @@ package com.github.colingrime.utils;
 
 import org.bukkit.Bukkit;
 
+import javax.annotation.Nonnull;
+import java.util.Objects;
 import java.util.logging.Level;
 
 public final class Logger {
 
-	private Logger() {}
-
-	public static void log(String message) {
+	public static void log(@Nonnull String message) {
+		Objects.requireNonNull(message);
 		Bukkit.getLogger().log(Level.INFO, "[SkyMines] " + message);
 	}
 
-	public static void severe(String message) {
+	public static void severe(@Nonnull String message) {
+		Objects.requireNonNull(message);
 		Bukkit.getLogger().log(Level.SEVERE, "[SkyMines] " + message);
 	}
 
-	public static void warn(String message) {
+	public static void warn(@Nonnull String message) {
+		Objects.requireNonNull(message);
 		Bukkit.getLogger().log(Level.WARNING, "[SkyMines] " + message);
+	}
+
+	private Logger() {
+		throw new UnsupportedOperationException("This class cannot be instantiated.");
 	}
 }
