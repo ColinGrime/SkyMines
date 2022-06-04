4cd7ccacee9f16bd50fde842d9ccb65bcda4803c
Scill <realscill@gmail.com>
2022-06-04 10:45:50 -0400

    Change to immutable list

--- src/main/java/com/github/colingrime/storage/sql/SchemaReader.java
+++ src/main/java/com/github/colingrime/storage/sql/SchemaReader.java
@@ -5,22 +5,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public final class SchemaReader {
 
-	private SchemaReader() {}
-
 	public static List<String> getQueries(InputStream is) {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
 			String query = reader.lines().collect(Collectors.joining("\n"));
-			return Arrays.asList(query.split(";"));
+			return List.of(query.split(";"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		return new ArrayList<>();
+	}
+
+	private SchemaReader() {
+		throw new UnsupportedOperationException("This class cannot be instantiated.");
 	}
 }
