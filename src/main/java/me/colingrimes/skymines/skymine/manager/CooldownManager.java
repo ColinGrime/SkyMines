babf68766d4fac9f2b92cd596ba2ab1d977e3292
Scill <realscill@gmail.com>
2025-03-28 15:39:15 -0400

    Add updated Cooldown system

---
+++ src/main/java/me/colingrimes/skymines/skymine/manager/CooldownManager.java
@@ -0,0 +1,30 @@
+package me.colingrimes.skymines.skymine.manager;
+
+import me.colingrimes.skymines.skymine.SkyMine;
+import me.colingrimes.midnight.cache.Cooldown;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
+import java.time.Duration;
+
+public class CooldownManager {
+
+	private final Cooldown<SkyMine> skyMineCooldown = Cooldown.create(Duration.ofSeconds(0));
+	private final Cooldown<Player> pickupCooldown = Cooldown.create(Duration.ofSeconds(300));
+	private final Cooldown<Player> placementCooldown = Cooldown.create(Duration.ofSeconds(10));
+
+	@Nonnull
+	public Cooldown<SkyMine> getSkyMineCooldown() {
+		return skyMineCooldown;
+	}
+
+	@Nonnull
+	public Cooldown<Player> getPickupCooldown() {
+		return pickupCooldown;
+	}
+
+	@Nonnull
+	public Cooldown<Player> getPlacementCooldown() {
+		return placementCooldown;
+	}
+}
