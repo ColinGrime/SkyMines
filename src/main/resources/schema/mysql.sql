a4bc50a6e7b85ff435a68e312b7d6b41767875a1
Scill <realscill@gmail.com>
2025-04-10 22:34:19 -0400

    Update schemas with new identifier + start utilizing json type

--- src/main/resources/schema/mysql.sql
+++ src/main/resources/schema/mysql.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS skymines_mines (
-    uuid      VARCHAR(36) NOT NULL,
-    owner     VARCHAR(36) NOT NULL,
-    structure TEXT        NOT NULL,
-    upgrades  TEXT        NOT NULL,
-    home      TEXT        NOT NULL,
+    uuid       VARCHAR(36) NOT NULL,
+    owner      VARCHAR(36) NOT NULL,
+    identifier TEXT        NOT NULL,
+    structure  JSON        NOT NULL,
+    upgrades   JSON        NOT NULL,
+    home       JSON        NOT NULL,
     PRIMARY KEY (uuid)
 );
--- src/main/resources/schema/postgresql.sql
+++ src/main/resources/schema/postgresql.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS skymines_mines (
-    uuid      UUID NOT NULL PRIMARY KEY,
-    owner     UUID NOT NULL,
-    structure TEXT NOT NULL,
-    upgrades  TEXT NOT NULL,
-    home      TEXT NOT NULL
+    uuid       UUID  NOT NULL PRIMARY KEY,
+    owner      UUID  NOT NULL,
+    identifier TEXT  NOT NULL,
+    structure  JSONB NOT NULL,
+    upgrades   JSONB NOT NULL,
+    home       JSONB NOT NULL
 );
