a4b3e2956988097ac9aa59d4668fec1769aa2ac9
Scill <realscill@gmail.com>
2025-04-20 22:35:07 -0400

    Add doc comments to cooldown manager

--- src/main/java/me/colingrimes/skymines/skymine/manager/CooldownManager.java
+++ src/main/java/me/colingrimes/skymines/skymine/manager/CooldownManager.java
@@ -13,16 +13,32 @@ public class CooldownManager {
 	private final Cooldown<Player> pickupCooldown = Cooldown.create(Duration.ofSeconds(300));
 	private final Cooldown<Player> throttle = Cooldown.create(Duration.ofMillis(100));
 
+	/**
+	 * Gets the skymine cooldown from resetting the mine.
+	 *
+	 * @return the skymine cooldown
+	 */
 	@Nonnull
 	public Cooldown<SkyMine> getSkyMineCooldown() {
 		return skyMineCooldown;
 	}
 
+	/**
+	 * Gets the player cooldown from picking up a skymine.
+	 *
+	 * @return the pickup cooldown
+	 */
 	@Nonnull
 	public Cooldown<Player> getPickupCooldown() {
 		return pickupCooldown;
 	}
 
+	/**
+	 * Gets the player cooldown from spam right-clicking the skymine token.
+	 * This is used to prevent potential duplication issues.
+	 *
+	 * @return the throttle
+	 */
 	@Nonnull
 	public Cooldown<Player> getThrottle() {
 		return throttle;
