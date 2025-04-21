42bb278209b93c4b780375231f1dac405c486d7a
Scill <realscill@gmail.com>
2025-04-20 22:35:17 -0400

    Add automatic mine reset logic

--- src/main/java/me/colingrimes/skymines/listener/SkyMineListeners.java
+++ src/main/java/me/colingrimes/skymines/listener/SkyMineListeners.java
@@ -4,19 +4,36 @@ import me.colingrimes.midnight.util.bukkit.Players;
 import me.colingrimes.skymines.api.SkyMineCooldownFinishEvent;
 import me.colingrimes.skymines.config.Messages;
 import me.colingrimes.skymines.config.Settings;
+import me.colingrimes.skymines.skymine.SkyMine;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import javax.annotation.Nonnull;
+import java.util.Optional;
 
 public class SkyMineListeners implements Listener {
 
 	@EventHandler
 	public void onCooldownFinish(@Nonnull SkyMineCooldownFinishEvent event) {
-		if (Settings.OPTION_COOLDOWN_NOTIFY_ON_RESET_FINISH.get()) {
-			Players.get(event.getSkyMine().getOwner()).ifPresent(player -> {
-				Messages.GENERAL_COOLDOWN_RESET_FINISH.replace("{id}", event.getSkyMine().getIndex()).send(player);
-			});
+		SkyMine skyMine = event.getSkyMine();
+		Optional<Player> player = Players.get(skyMine.getOwner());
+		if (player.isEmpty()) {
+			return;
+		}
+
+		// Automatic reset.
+		if (Settings.OPTION_RESET_AUTOMATIC.get() && player.get().hasPermission("skymines.reset.automatic")) {
+			skyMine.reset(false);
+			Messages.SUCCESS_RESET_AUTOMATIC.send(player.get());
+			if (Settings.OPTION_RESET_TELEPORT_HOME.get()) {
+				skyMine.getStructure().getPlayers().forEach(p -> p.teleport(skyMine.getHome().toLocation()));
+			}
+		}
+
+		// Notify on reset finish.
+		else if (Settings.OPTION_COOLDOWN_NOTIFY_ON_RESET_FINISH.get()) {
+			Messages.GENERAL_COOLDOWN_RESET_FINISH.replace("{id}", skyMine.getIndex()).send(player.get());
 		}
 	}
 }
